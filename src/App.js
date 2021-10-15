c58e08344569f21dfabd890e7066c1d3ffc43726
CamiloGarrido <camilogarrido12@gmail.com>
2021-10-15 20:05:47 -0300

    se agrega lazy load

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router";
 import { Container } from "@mui/material";
-import Home from "./view/home/Home";
-import NotFound from "./view/notFound/NotFound";
+
 import Header from "./component/Header";
-import Pokemon from "./view/pokemones/Pokemon";
-import PokemonDetail from "./view/pokemones/PokemonDetail";
-import Bayas from "./view/bayas/Bayas";
-import Attack from "./view/attack/Attack";
-import BayasDetail from "./view/bayas/BayasDetail";
-import Pokeballs from "./view/pokeball/Pokeballs";
-import PokeballDetail from "./view/pokeball/PokeballDetail";
-import AttackDetail from "./view/attack/AttackDetail";
+
+const Home = React.lazy(() => import("./view/home/Home"));
+const NotFound = React.lazy(() => import("./view/notFound/NotFound"));
+const Pokemon = React.lazy(() => import("./view/pokemones/Pokemon"));
+const PokemonDetail = React.lazy(() =>
+  import("./view/pokemones/PokemonDetail")
+);
+const Bayas = React.lazy(() => import("./view/bayas/Bayas"));
+const Attack = React.lazy(() => import("./view/attack/Attack"));
+const BayasDetail = React.lazy(() => import("./view/bayas/BayasDetail"));
+const Pokeballs = React.lazy(() => import("./view/pokeball/Pokeballs"));
+const PokeballDetail = React.lazy(() =>
+  import("./view/pokeball/PokeballDetail")
+);
+const AttackDetail = React.lazy(() => import("./view/attack/AttackDetail"));
 
 const App = () => {
   return (
     <>
       <Header />
       <Container>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/pokemones" component={Pokemon} />
-          <Route exact path="/pokeballs" component={Pokeballs} />
-          <Route exact path="/pokeball/:id" component={PokeballDetail} />
-          <Route exact path="/berrys" component={Bayas} />
-          <Route exact path="/berrys/:id" component={BayasDetail} />
-          <Route exact path="/attack" component={Attack} />
-          <Route exact path="/attack/:id" component={AttackDetail} />
-          <Route exact path="/pokemon/:id" component={PokemonDetail} />
-          <Route path="*" component={NotFound} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/pokemones" component={Pokemon} />
+            <Route exact path="/pokeballs" component={Pokeballs} />
+            <Route exact path="/pokeball/:id" component={PokeballDetail} />
+            <Route exact path="/berrys" component={Bayas} />
+            <Route exact path="/berrys/:id" component={BayasDetail} />
+            <Route exact path="/attack" component={Attack} />
+            <Route exact path="/attack/:id" component={AttackDetail} />
+            <Route exact path="/pokemon/:id" component={PokemonDetail} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Suspense>
       </Container>
       <div className="divFooter"></div>
       <footer>ShadowLine R</footer>
